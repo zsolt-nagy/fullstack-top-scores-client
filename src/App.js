3468a60cb0fc512babf61c7de51bdb131d9ef705
Zsolt Nagy <info@zsoltnagy.eu>
2023-06-15 23:07:34 +0200

    endpoint hierarchy and README

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const [scores, setScores] = useState([]);
 
     function loadData() {
-        fetch("https://xxfydv-8080.csb.app/api/list/")
+        fetch("https://xxfydv-8080.csb.app/api/players/")
             .then((x) => x.json())
             .then((response) => {
                 setScores(response);
@@ -17,7 +17,7 @@ function App() {
     useEffect(loadData, []);
 
     function addScore(name, score) {
-        fetch("https://xxfydv-8080.csb.app/api/list/new", {
+        fetch("https://xxfydv-8080.csb.app/api/players/new", {
             method: "POST",
             body: JSON.stringify({
                 name,
@@ -33,7 +33,7 @@ function App() {
     }
 
     function deleteScore(id) {
-        fetch(`https://xxfydv-8080.csb.app/api/list/${id}/delete`, {
+        fetch(`https://xxfydv-8080.csb.app/api/players/${id}/delete`, {
             method: "DELETE",
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
